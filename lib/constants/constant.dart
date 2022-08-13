d7c7de8d55a70b9a7aff85aaf51440b358b855d6
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-08-14 02:15:21 +0700

    add: constants

---
+++ lib/constants/constant.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF2B2B2B);
+const Color cream = Color(0xFFFDCB6E);
+const Color grey = Color(0xFFD2D2D2);
+const Color darkgrey = Color(0xFF686868);
+
+const TextStyle regular = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle medium = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle semiBold = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle bold = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.bold,
+);
