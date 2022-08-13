f2acb0530b29ac6bc86cebdd705cdc9cf122d178
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-08-14 02:16:13 +0700

    add: category widget

---
+++ lib/widgets/categories_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:kopiku/constants/constant.dart';
+
+class CategoriesWidget extends StatefulWidget {
+  const CategoriesWidget({Key? key}) : super(key: key);
+
+  static List<String> categories = [
+    'Espresso',
+    'Brewed',
+    'Blended',
+    'Others',
+  ];
+
+  @override
+  State<CategoriesWidget> createState() => _CategoriesWidgetState();
+}
+
+class _CategoriesWidgetState extends State<CategoriesWidget> {
+  final _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 32,
+      child: Scrollbar(
+        controller: _scrollController,
+        child: ListView(
+          controller: _scrollController,
+          scrollDirection: Axis.horizontal,
+          children: List.generate(
+            CategoriesWidget.categories.length,
+            (index) => Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              margin: EdgeInsets.only(left: index == 0 ? 24 : 16, right: 16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: index == 0 ? cream : white,
+              ),
+              child: Center(
+                child: Text(
+                  CategoriesWidget.categories[index],
+                  style: medium.copyWith(
+                      color: index == 0 ? black : grey, fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
