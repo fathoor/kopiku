9631d26c1ccb83bfd8bfd176775437a9445d3ec2
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-08-14 12:16:19 +0700

    add: initial detail screen

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:kopiku/model/coffee.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Coffee coffee;
+
+  const DetailScreen({Key? key, required this.coffee}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
