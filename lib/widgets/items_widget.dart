3a91a91798ea1f6a0b6447af6b89d66b28cd33f6
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-08-14 12:48:06 +0700

    edit: scrollbar

--- lib/widgets/items_widget.dart
+++ lib/widgets/items_widget.dart
@@ -6,74 +6,85 @@ import 'package:kopiku/model/coffee.dart';
 import 'package:kopiku/screens/detail_screen.dart';
 import 'package:kopiku/widgets/favorites_widget.dart';
 
-class ItemsWidget extends StatelessWidget {
+class ItemsWidget extends StatefulWidget {
   const ItemsWidget({Key? key}) : super(key: key);
 
   @override
+  State<ItemsWidget> createState() => _ItemsWidgetState();
+}
+
+class _ItemsWidgetState extends State<ItemsWidget> {
+  final _scrollController = ScrollController();
+
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: List.generate(
-          coffeeList.length,
-          (index) => GestureDetector(
-            onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DetailScreen(coffee: coffeeList[index]),
-              ),
-            ),
-            child: Container(
-              margin: EdgeInsets.only(left: index == 0 ? 24 : 16, right: 16),
-              width: 256,
-              height: 384,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  image: AssetImage(coffeeList[index].image),
-                  fit: BoxFit.cover,
+    return Scrollbar(
+      controller: _scrollController,
+      child: SingleChildScrollView(
+        controller: _scrollController,
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: List.generate(
+            coffeeList.length,
+            (index) => GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(coffee: coffeeList[index]),
                 ),
               ),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  margin: const EdgeInsets.all(8),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 16,
+              child: Container(
+                margin: EdgeInsets.only(left: index == 0 ? 24 : 16, right: 16),
+                width: 256,
+                height: 384,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: AssetImage(coffeeList[index].image),
+                    fit: BoxFit.cover,
                   ),
-                  width: double.infinity,
-                  height: 64,
-                  decoration: BoxDecoration(
-                    color: black.withOpacity(0.5),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            coffeeList[index].name,
-                            style: bold.copyWith(
-                              color: white,
-                              fontSize: 16,
+                ),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    margin: const EdgeInsets.all(8),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 16,
+                    ),
+                    width: double.infinity,
+                    height: 64,
+                    decoration: BoxDecoration(
+                      color: black.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              coffeeList[index].name,
+                              style: bold.copyWith(
+                                color: white,
+                                fontSize: 16,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            'Rp${coffeeList[index].price}',
-                            style: medium.copyWith(
-                              color: white,
-                              fontSize: 12,
-                            ),
-                          )
-                        ],
-                      ),
-                      const FavoriteWidget(),
-                    ],
+                            const SizedBox(height: 8),
+                            Text(
+                              'Rp${coffeeList[index].price}',
+                              style: medium.copyWith(
+                                color: white,
+                                fontSize: 12,
+                              ),
+                            )
+                          ],
+                        ),
+                        const FavoriteWidget(),
+                      ],
+                    ),
                   ),
                 ),
               ),
