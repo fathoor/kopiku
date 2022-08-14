eddd55e6bf8e82a5796b8e1cb2c049b918688423
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-08-14 15:04:17 +0700

    fix: category list aligned center

--- lib/widgets/categories_widget.dart
+++ lib/widgets/categories_widget.dart
@@ -24,23 +24,25 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
       height: 32,
       child: Scrollbar(
         controller: _scrollController,
-        child: ListView(
+        child: SingleChildScrollView(
           controller: _scrollController,
           scrollDirection: Axis.horizontal,
-          children: List.generate(
-            CategoriesWidget.categories.length,
-            (index) => Container(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              margin: EdgeInsets.only(left: index == 0 ? 24 : 16, right: 16),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: index == 0 ? cream : white,
-              ),
-              child: Center(
-                child: Text(
-                  CategoriesWidget.categories[index],
-                  style: medium.copyWith(
-                      color: index == 0 ? black : grey, fontSize: 16),
+          child: Row(
+            children: List.generate(
+              CategoriesWidget.categories.length,
+              (index) => Container(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                margin: EdgeInsets.only(left: index == 0 ? 24 : 16, right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: index == 0 ? cream : white,
+                ),
+                child: Center(
+                  child: Text(
+                    CategoriesWidget.categories[index],
+                    style: medium.copyWith(
+                        color: index == 0 ? black : grey, fontSize: 16),
+                  ),
                 ),
               ),
             ),
