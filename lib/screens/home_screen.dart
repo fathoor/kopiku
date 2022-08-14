ad4cc7128615d66127c8416585bcd4d472752b3e
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-08-14 15:06:38 +0700

    edit: home screen for web

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -126,7 +126,7 @@ class MobileHome extends StatelessWidget {
                     ),
                     const SizedBox(height: 16),
                     Text(
-                      'Ceriakan dan Warnai Hari\nBersama Kopi Pilihanmu',
+                      'Ceriakan dan Warnai Hari Bersama Kopi Pilihanmu',
                       style: bold.copyWith(
                         color: black,
                         fontSize: 26,
@@ -224,18 +224,214 @@ class MobileHome extends StatelessWidget {
   }
 }
 
-class WebHome extends StatefulWidget {
+class WebHome extends StatelessWidget {
   const WebHome({Key? key}) : super(key: key);
 
   @override
-  State<WebHome> createState() => _WebHomeState();
-}
-
-class _WebHomeState extends State<WebHome> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('A'),
+      backgroundColor: white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: 960,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.coffee_rounded,
+                                color: cream,
+                                size: 36,
+                              ),
+                              const SizedBox(width: 16),
+                              Text(
+                                'Kopi',
+                                style: bold.copyWith(
+                                  color: black,
+                                  fontSize: 24,
+                                ),
+                              ),
+                              Text(
+                                'Ku',
+                                style: bold.copyWith(
+                                  color: cream,
+                                  fontSize: 24,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Selamat Datang, ',
+                                    style: semiBold.copyWith(
+                                      color: darkgrey,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Fathoor',
+                                    style: semiBold.copyWith(
+                                      color: cream,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(
+                                    '!',
+                                    style: semiBold.copyWith(
+                                      color: darkgrey,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(width: 16),
+                              Container(
+                                width: 50,
+                                height: 50,
+                                padding: const EdgeInsets.all(3),
+                                decoration: const BoxDecoration(
+                                  color: black,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Container(
+                                  width: 48,
+                                  height: 48,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: AssetImage(
+                                        'assets/images/fathur.png',
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 32),
+                      Center(
+                        child: Text(
+                          'Ceriakan dan Warnai Hari Bersama Kopi Pilihanmu',
+                          style: bold.copyWith(
+                            color: black,
+                            fontSize: 26,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 32),
+                      Center(
+                        child: SizedBox(
+                          width: 640,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: SizedBox(
+                                  height: 48,
+                                  child: TextField(
+                                    cursorColor: black,
+                                    style: regular.copyWith(
+                                      color: black,
+                                    ),
+                                    decoration: InputDecoration(
+                                      prefixIcon: const Icon(
+                                        Icons.search,
+                                        color: grey,
+                                      ),
+                                      hintText: 'Cari',
+                                      hintStyle: const TextStyle(
+                                        color: grey,
+                                      ),
+                                      contentPadding:
+                                          const EdgeInsets.symmetric(
+                                              horizontal: 16),
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        borderSide: const BorderSide(
+                                          color: grey,
+                                        ),
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        borderSide: const BorderSide(
+                                          color: grey,
+                                        ),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                        borderSide: const BorderSide(
+                                          color: grey,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 16),
+                              Container(
+                                width: 48,
+                                height: 48,
+                                decoration: BoxDecoration(
+                                  color: black,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Icon(
+                                  Icons.filter_list_rounded,
+                                  color: grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Kategori',
+                      style: bold.copyWith(
+                        color: black,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              const CategoriesWidget(),
+              const SizedBox(height: 24),
+              const ItemsWidget(),
+              const SizedBox(height: 24),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
