a49f94039871f47c1e7148cd0c7f4a5a5c93ea4e
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-08-14 13:21:51 +0700

    edit: coffee detail

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:kopiku/constants/constant.dart';
 import 'package:kopiku/model/coffee.dart';
+import 'package:kopiku/widgets/favorites_widget.dart';
 
 class DetailScreen extends StatelessWidget {
   final Coffee coffee;
@@ -8,6 +12,147 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(coffee.image),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 24,
+                    horizontal: 24,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: Container(
+                          width: 36,
+                          height: 36,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 13,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: white.withOpacity(0.5),
+                          ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              size: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Espresso',
+                        style: semiBold.copyWith(
+                          color: white,
+                          fontSize: 15,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 36,
+                        width: 36,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  margin: const EdgeInsets.all(24),
+                  padding: const EdgeInsets.all(16),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: black.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: black.withOpacity(0.2),
+                        offset: const Offset(0, 0),
+                        blurRadius: 16,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            coffee.name,
+                            style: bold.copyWith(
+                              color: white,
+                              fontSize: 22,
+                            ),
+                          ),
+                          const FavoriteWidget(),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        coffee.detail,
+                        style: regular.copyWith(
+                          color: white,
+                          fontSize: 14,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Rp${coffee.price}',
+                            style: semiBold.copyWith(
+                              color: white,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 36,
+                        decoration: BoxDecoration(
+                          color: cream,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Center(
+                            child: Text(
+                          'Pesan',
+                          style: semiBold.copyWith(
+                            color: black,
+                            fontSize: 18,
+                          ),
+                        )),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
