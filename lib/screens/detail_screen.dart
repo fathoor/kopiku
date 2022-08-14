0e9c75d1ae78ac47a8f6b989b13cfc044297a578
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-08-14 16:12:35 +0700

    edit: detail screen for web

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:kopiku/constants/constant.dart';
 import 'package:kopiku/model/coffee.dart';
@@ -9,6 +7,25 @@ class DetailScreen extends StatelessWidget {
   final Coffee coffee;
 
   const DetailScreen({Key? key, required this.coffee}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 900) {
+          return WebDetail(coffee: coffee);
+        } else {
+          return MobileDetail(coffee: coffee);
+        }
+      },
+    );
+  }
+}
+
+class MobileDetail extends StatelessWidget {
+  final Coffee coffee;
+
+  const MobileDetail({Key? key, required this.coffee}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -112,11 +129,10 @@ class DetailScreen extends StatelessWidget {
                           color: white,
                           fontSize: 14,
                         ),
-                        textAlign: TextAlign.justify,
                       ),
                       const SizedBox(height: 16),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
                           Text(
@@ -129,21 +145,25 @@ class DetailScreen extends StatelessWidget {
                         ],
                       ),
                       const SizedBox(height: 16),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 36,
-                        decoration: BoxDecoration(
-                          color: cream,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Center(
-                            child: Text(
-                          'Pesan',
-                          style: semiBold.copyWith(
-                            color: black,
-                            fontSize: 18,
+                      GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 36,
+                          decoration: BoxDecoration(
+                            color: cream,
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                        )),
+                          child: Center(
+                            child: Text(
+                              'Pesan',
+                              style: semiBold.copyWith(
+                                color: black,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
                     ],
                   ),
@@ -151,6 +171,171 @@ class DetailScreen extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class WebDetail extends StatelessWidget {
+  final Coffee coffee;
+
+  const WebDetail({Key? key, required this.coffee}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 24,
+                  horizontal: 24,
+                ),
+                child: SizedBox(
+                  width: 960,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: Container(
+                          width: 36,
+                          height: 36,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 13,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: black.withOpacity(0.5),
+                          ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: white,
+                              size: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Espresso',
+                        style: semiBold.copyWith(
+                          color: black,
+                          fontSize: 15,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 36,
+                        width: 36,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 64),
+                width: 512,
+                height: 768,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: AssetImage(coffee.image),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    margin: const EdgeInsets.all(24),
+                    padding: const EdgeInsets.all(16),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: black.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: black.withOpacity(0.2),
+                          offset: const Offset(0, 0),
+                          blurRadius: 16,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              coffee.name,
+                              style: bold.copyWith(
+                                color: white,
+                                fontSize: 22,
+                              ),
+                            ),
+                            const FavoriteWidget(),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          coffee.detail,
+                          style: regular.copyWith(
+                            color: white,
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Rp${coffee.price}',
+                              style: semiBold.copyWith(
+                                color: white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 16),
+                        GestureDetector(
+                          onTap: () => Navigator.pop(context),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 36,
+                            decoration: BoxDecoration(
+                              color: cream,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Pesan',
+                                style: semiBold.copyWith(
+                                  color: black,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
