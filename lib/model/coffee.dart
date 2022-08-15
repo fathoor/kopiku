fc676dfc2f6474dc60975142063500f8eca3c078
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-08-15 10:10:13 +0700

    edit: more dummy data

--- lib/model/coffee.dart
+++ lib/model/coffee.dart
@@ -1,8 +1,8 @@
 class Coffee {
-  String name;
-  String detail;
-  String price;
-  String image;
+  final name;
+  final detail;
+  final price;
+  final image;
 
   Coffee({
     required this.name,
@@ -11,6 +11,14 @@ class Coffee {
     required this.image,
   });
 }
+
+var categoryList = [
+  'Semua',
+  'Espresso',
+  'Brewed',
+  'Blended',
+  'Lainnya',
+];
 
 var coffeeList = [
   Coffee(
@@ -32,5 +40,123 @@ var coffeeList = [
         'With the slight dab of foam we set atop our signature espresso in this classic European-style beverage.',
     price: '17.999',
     image: 'assets/images/espresso.jpg',
+  ),
+  Coffee(
+    name: 'Caffè Americano',
+    detail: 'Rich, full-bodied espresso with hot water.',
+    price: '12.999',
+    image: 'assets/images/americano.jpg',
+  ),
+  Coffee(
+    name: 'Brewed Coffee',
+    detail: 'Brewed perfectly for those who enjoy their coffee black.',
+    price: '11.999',
+    image: 'assets/images/brewed.jpg',
+  ),
+  Coffee(
+    name: 'Misto',
+    detail: 'Equal parts fresh filter coffee and frothy steamed milk.',
+    price: '14.999',
+    image: 'assets/images/misto.jpg',
+  ),
+  Coffee(
+    name: 'Cold Brew',
+    detail: 'Slow-steeped, small-batch and super smooth.',
+    price: '17.999',
+    image: 'assets/images/coldbrew.jpg',
+  ),
+  Coffee(
+    name: 'Coffee Frappuccino',
+    detail: 'Blended with nonfat milk and ice for a sip on the light side.',
+    price: '19.999',
+    image: 'assets/images/coffeefrap.jpg',
+  ),
+  Coffee(
+    name: 'Mocha Frappuccino',
+    detail: 'Roast coffee, milk, and ice all come together for a mocha flavor.',
+    price: '19.999',
+    image: 'assets/images/mochafrap.jpg',
+  ),
+  Coffee(
+    name: 'Signature',
+    detail:
+        'Chocolate and dairy milk, steamed together to create a hot chocolate fit for a chocaholic!',
+    price: '23.999',
+    image: 'assets/images/signature.jpg',
+  ),
+];
+
+var espressoList = [
+  Coffee(
+    name: 'Cappuccino',
+    detail: 'Espresso with steamed milk, topped with a deep layer of foam.',
+    price: '14.999',
+    image: 'assets/images/cappuccino.jpg',
+  ),
+  Coffee(
+    name: 'Caramel Macchiato',
+    detail:
+        'Espresso combined with vanilla-flavoured syrup, milk and caramel sauce over ice.',
+    price: '19.999',
+    image: 'assets/images/caramel.jpg',
+  ),
+  Coffee(
+    name: 'Espresso Macchiato',
+    detail:
+        'With the slight dab of foam we set atop our signature espresso in this classic European-style beverage.',
+    price: '17.999',
+    image: 'assets/images/espresso.jpg',
+  ),
+  Coffee(
+    name: 'Caffè Americano',
+    detail: 'Rich, full-bodied espresso with hot water.',
+    price: '12.999',
+    image: 'assets/images/americano.jpg',
+  ),
+];
+
+var brewedList = [
+  Coffee(
+    name: 'Brewed Coffee',
+    detail: 'Brewed perfectly for those who enjoy their coffee black.',
+    price: '11.999',
+    image: 'assets/images/brewed.jpg',
+  ),
+  Coffee(
+    name: 'Misto',
+    detail: 'Equal parts fresh filter coffee and frothy steamed milk.',
+    price: '14.999',
+    image: 'assets/images/misto.jpg',
+  ),
+  Coffee(
+    name: 'Cold Brew',
+    detail: 'Slow-steeped, small-batch and super smooth.',
+    price: '17.999',
+    image: 'assets/images/coldbrew.jpg',
+  ),
+];
+
+var blendedList = [
+  Coffee(
+    name: 'Coffee Frappuccino',
+    detail: 'Blended with nonfat milk and ice for a sip on the light side.',
+    price: '19.999',
+    image: 'assets/images/coffeefrap.jpg',
+  ),
+  Coffee(
+    name: 'Mocha Frappuccino',
+    detail: 'Roast coffee, milk, and ice all come together for a mocha flavor.',
+    price: '19.999',
+    image: 'assets/images/mochafrap.jpg',
+  ),
+];
+
+var otherList = [
+  Coffee(
+    name: 'Signature',
+    detail:
+        'Chocolate and dairy milk, steamed together to create a hot chocolate fit for a chocaholic!',
+    price: '23.999',
+    image: 'assets/images/signature.jpg',
   ),
 ];
